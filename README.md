fde72911c0a4dd52bcc0709ce2e16b55c4ad9304
Sparkenstein <zetabytes.pp@gmail.com>
2018-12-15 22:22:45 +0530

    added env checks

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ This is just a wrapper around the axios to make a multipart/form-data request to
 ### Usage
  - install the tool with `npm install -g sprk-bin`
  - set these two ENV variables with your favourite method:
-   - SPRK_SERVER_URL = 'http://example.com/'
-   - SPRK_API_KEY = 'sampleAPIKey'
+   - SPRK_SERVER_URL=http://example.com/
+   - SPRK_API_KEY=sampleAPIKey
  - Then you are good to upload any file from any folder to your private instance of `sprk`
 
 -----
--- index.js
+++ index.js
@@ -12,6 +12,17 @@ if(!fileName){
     process.exit(0);
 }
 
+if(!URL){
+    console.error(`Server URL is not set, Please set ${SPRK_SERVER_URL} env variable`);
+    process.exit(0);
+}
+
+if(!SPRK_API_KEY){
+    console.error(`Server API key is not set, Please set ${SPRK_API_KEY} env variable.
+    it can be anything but it should match the "sprk" instance API key you setup`);
+    process.exit(0);
+}
+
 const fileData = createReadStream(fileName);
 form.append('file', fileData);
 
